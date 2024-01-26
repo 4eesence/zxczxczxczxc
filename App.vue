421db16c35bb19daa155714237c97897818dd6c7
4essence <148618054+4eesence@users.noreply.github.com>
2024-01-26 21:51:36 +0300

    Update App.vue

--- App.vue
+++ App.vue
@@ -1,4 +1,4 @@
-<!-- <template>
+<template>
   <div>
     <input type="text" v-model="newItemText">
     <button @click="addItem">Жмякай</button>
@@ -25,49 +25,4 @@ export default {
     }
   }
 };
-</script> -->
-
-
-
-<template>
-  <div :class="{ 'card': true, 'favorite': isFavorite }">
-    <img :src="product.image" :alt="product.name" />
-    <h3>{{ product.name }}</h3>
-    <p>{{ product.description }}</p>
-    <button @click="toggleFavorite">
-      {{ isFavorite ? 'Remove from Favorites' : 'Add to Favorites' }}
-    </button>
-  </div>
-</template>
-
-<script>
-import { ref } from 'vue';
-
-export default {
-  props: {
-    product: Object,
-  },
-  setup(props) {
-    const isFavorite = ref(false);
-
-    const toggleFavorite = () => {
-      isFavorite.value = !isFavorite.value;
-      // Здесь можно добавить логику для добавления в избранное в родительском компоненте
-    };
-
-    return { isFavorite, toggleFavorite };
-  }
-};
 </script>
-
-<style>
-.card {
-  border: 1px solid #ccc;
-  padding: 10px;
-  margin: 10px;
-}
-
-.favorite {
-  background-color: lightyellow;
-}
-</style>
